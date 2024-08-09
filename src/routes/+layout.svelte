1abd212e0a828d261d56f4d5c4d03f5adb3e68a6
Kolomona <kolomona@kolomona.com>
2024-08-08 23:02:26 -0500

    Added some styles in an attempt to make it look better on my phone

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,21 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/search">Music Search</a>
-    <a href="/tagSearch">Tag Search</a>
-    <a href="/tagBrowse">Browse Tags</a>
-</nav>
-<div id="disclaimer">
-    <p><b>Disclaimer:</b> This is a work in progress. it will likely break. <br>We are running with Scissors. 🙂 </p>
-    
+<script>
+	import "../app.css";
+</script>
+<div id="layout" class="container">
+	<nav>
+		<a href="/">Home</a> | 
+		<a href="/search">Music Search</a> | 
+		<a href="/tagSearch">Tag Search</a> | 
+		<a href="/tagBrowse">Browse Tags</a>
+	</nav>
+	<div id="disclaimer">
+		<p><b>Disclaimer:</b> This is a work in progress. it will likely break. <br>We are running with Scissors. 🙂 </p>
+		
+	</div>
+	<slot />
+	<hr>
+	<p>This project is a proof of concept for how an interface to the <a href="https://github.com/v4vmusic/v4vtags">v4vtags api</a> may function.</p>
+	<p>This is open source: <a href="https://github.com/v4vmusic">v4vmusic repos Github</a></p>
+	<p>Send some Value for Value <a href="https://getalby.com/p/kolomona"><img src="https://cdn.kolomona.com/media/images/kolomona-alby.svg" alt="" width="150px"></a></p>
+
 </div>
-<slot />
-<hr>
-<p>This project is a proof of concept for how an interface to the <a href="https://github.com/v4vmusic/v4vtags">v4vtags api</a> may function.</p>
-<p>This is open source: <a href="https://github.com/v4vmusic">v4vmusic repos Github</a></p>
-<p>Send some Value for Value <a href="https://getalby.com/p/kolomona"><img src="https://cdn.kolomona.com/media/images/kolomona-alby.svg" alt="" width="150px"></a></p>
